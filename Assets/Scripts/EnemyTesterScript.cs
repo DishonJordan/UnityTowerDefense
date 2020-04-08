58fcae13fb7f6acb0cf24e1c0d93d92f2d1b4926
DishonJordan <dishonjordan@hotmail.com>
2020-04-07 20:41:54 -0700

    created a simple enemy movement script (in place until enemies are actually in the game)

---
+++ Assets/Scripts/EnemyTesterScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTesterScript : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(0f, 0f, 15f), step);
+    }
+}
