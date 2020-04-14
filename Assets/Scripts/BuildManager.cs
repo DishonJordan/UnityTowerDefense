7745e2827cb4ebd4a75475048f528743945f5efb
DishonJordan <dishonjordan@hotmail.com>
2020-04-14 14:59:29 -0700

    added a build manager to handle the placing of turrets

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -1,23 +1,59 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class BuildManager : MonoBehaviour
 {
     public GameObject tileShopUI;
+    public static bool uiIsActive;
 
-    public GameObject turretToBuild;
     private GameObject turretOnTile;
+
+    public Material highlightColor;
+    private Color originalColor;
+    private Renderer myRenderer;
 
     private Vector3 offset = new Vector3(0f, 0.2f, 0f);
 
-    void OnMouseDown()
+    private void Start()
     {
-        if (turretToBuild != null && turretOnTile == null ) { //Will need to also check money later
-            BuildTurret();
+        uiIsActive = false;
+        myRenderer = GetComponent<Renderer>();
+        originalColor = myRenderer.materials[1].color;
+    }
+
+    private void OnMouseEnter()
+    {
+        if (!uiIsActive && turretOnTile == null)
+        {
+            myRenderer.materials[1].color = highlightColor.color;
         }
     }
 
-    void BuildTurret() {
-        turretOnTile = Instantiate(turretToBuild, transform.position + offset, transform.rotation);
+    private void OnMouseExit()
+    {
+        if (!uiIsActive || turretOnTile == null)
+        {
+            myRenderer.materials[1].color = originalColor;
+        }
     }
 
+    private void OnMouseDown()
+    {
+        if (turretOnTile == null && !EventSystem.current.IsPointerOverGameObject() && !uiIsActive)
+        {
+            tileShopUI.SetActive(!tileShopUI.activeSelf);
+            uiIsActive = tileShopUI.activeSelf;
+            myRenderer.materials[1].color = originalColor;
+        }
+    }
+
+    public void BuildTurret(GameObject turret) {
+        Debug.Log(turret);
+        //Need to check money also
+        if (turret != null && turretOnTile == null){ 
+            turretOnTile = (GameObject)Instantiate(turret, transform.position + offset, transform.rotation);
+            tileShopUI.SetActive(false);
+            uiIsActive = false;
+        }
+    }
 }
