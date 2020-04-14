e6687239e295644a0844b931ced9a9b91e7af3bd
Kevin Huang <khuan028@ucr.edu>
2020-04-14 15:43:24 -0700

    Removed singleton pattern from Bank class.

--- Assets/Scripts/Bank.cs
+++ Assets/Scripts/Bank.cs
@@ -1,28 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Bank : MonoBehaviour
 {
-	#region Singleton
-	public static Bank instance { get; private set; }
-
-    private void Awake()
-    {
-        if(instance == null)
-        {
-            instance = this;
-            // If we change scenes, the bank will stay around.
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Debug.LogWarning("Destroying Bank because one exists already.");
-            Destroy(this);
-        }
-    }
-
-	#endregion
-
 	public int money;
 }
