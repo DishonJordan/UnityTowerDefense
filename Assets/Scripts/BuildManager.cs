c02f1380eda9dd50546bfd1c3c3d911695a6484a
DishonJordan <dishonjordan@hotmail.com>
2020-04-15 11:40:07 -0700

    implemented build manager

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BuildManager : MonoBehaviour
+{
+    public static bool shopUIActive;
+
+    [Header("UI GameObjects")]
+    public GameObject turretShopUI;
+    public GameObject upgradeUI; // To be implemented
+
+    [Header("Material")]
+    public Material highlightColor;
+
+    private GameObject turretOnTile;
+    private Color originalColor;
+    private Renderer myRenderer;
+    private Vector3 offset = new Vector3(0f, 0.2f, 0f);
+
+
+    private void Start()
+    {
+        shopUIActive = false;
+        myRenderer = GetComponent<Renderer>();
+        originalColor = myRenderer.materials[1].color;
+    }
+
+    /* Changes the color of tile to show it is clickable */
+    private void OnMouseEnter()
+    {
+        if (!shopUIActive && turretOnTile == null)
+        {
+            myRenderer.materials[1].color = highlightColor.color;
+        }
+    }
+
+    /* Reverts the color of the Tile */
+    private void OnMouseExit()
+    {
+        if (!shopUIActive || turretOnTile == null)
+        {
+            myRenderer.materials[1].color = originalColor;
+        }
+    }
+
+    /* Activates the shop UI */
+    private void OnMouseDown()
+    {
+        if (turretOnTile == null && !EventSystem.current.IsPointerOverGameObject() && !shopUIActive)
+        {
+            EnableShopUI();
+        }
+    }
+
+    /* This is called by the onclick event of the turretShopUI turret button */
+    public void BuildTurret(GameObject turret)
+    {
+        if (turret != null && turretOnTile == null)
+        { // Need to also check money
+            turretOnTile = (GameObject)Instantiate(turret, transform.position + offset, transform.rotation);
+            turretShopUI.SetActive(false);
+            shopUIActive = false;
+        }
+    }
+
+    /* Enables the shop UI */
+    public void EnableShopUI()
+    {
+        turretShopUI.SetActive(true);
+        shopUIActive = turretShopUI.activeSelf;
+        myRenderer.materials[1].color = originalColor;
+    }
+
+    /* Disables the shop UI */
+    public void DisableShopUI()
+    {
+        turretShopUI.SetActive(false);
+        shopUIActive = turretShopUI.activeSelf;
+    }
+}
