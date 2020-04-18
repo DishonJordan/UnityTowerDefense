7e75f6c3dbf3f8bf4d7536eb56c1d8497b432d71
DishonJordan <dishonjordan@hotmail.com>
2020-04-18 09:38:03 -0700

    made the shop ui track the main camera

---
+++ Assets/Scripts/PointTowardsCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PointTowardsCamera : MonoBehaviour
+{
+    public static GameObject mainCamera;
+    private readonly float turnRate = 3f;
+
+    private void Start()
+    {
+        /* Only called once by all tiles */
+        if (mainCamera == null)
+        {
+            mainCamera = GameObject.Find("Main Camera");
+        }
+    }
+
+    void Update()
+    {
+        Vector3 direction = mainCamera.transform.position - transform.position;
+        Quaternion qRotation = Quaternion.LookRotation(direction);
+        Vector3 rotation = Quaternion.Lerp(transform.rotation, qRotation, Time.deltaTime * turnRate).eulerAngles;
+
+        /* Rotates the ui about the y axis*/
+        transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+}
