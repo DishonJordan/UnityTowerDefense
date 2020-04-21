88b8cad72983109c9782f196403c316b246eb145
asori015 <32408013+asori015@users.noreply.github.com>
2020-04-20 19:48:24 -0700

    Greated GameManager script to handle basehealth.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    int baseHealth;
+
+    public int Health
+	{
+		get => baseHealth;
+
+		private set
+		{
+			baseHealth = value;
+			if(baseHealth <= 0){
+                GameEnded();
+            }
+		}
+	}
+
+    public void DecrementBy(int amount){
+        Health -= amount;
+    }
+
+    void GameEnded(){
+        Debug.Log("Game has ended");
+    }
+}
