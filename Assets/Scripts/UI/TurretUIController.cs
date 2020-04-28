ef229a95d166c9ce940b9e52e155554d17b522cf
Kevin Huang <khuan028@ucr.edu>
2020-04-28 16:37:49 -0700

    TurretUI prefab now auto-finds its dependencies as long as it is a child of a Turret object.

--- Assets/Scripts/UI/TurretUIController.cs
+++ Assets/Scripts/UI/TurretUIController.cs
@@ -1,52 +1,84 @@
-﻿using TMPro;
+﻿using System.Linq;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Assertions;
 
 public class TurretUIController : MonoBehaviour
 {
-    [Header("GameObjects")]
-    public GameObject turret;
-    public Button[] buttons;
+    [Header("Turret")]
+    public Turret turret;       // If null, script will find it in parent
 
-    public TextMeshProUGUI[] texts;
+    [Header("Buttons")]
+    public Button sellButton;
+    public Button upgradeButton;
+    public Button repairButton;
+    public Button exitButton;
 
     [Header("Sprites")]
     public Sprite canPurchaseSprite;
     public Sprite cannotPurchaseSprite;
 
-    private Turret turretScript;
-    private int[] prices;
+    private List<int> prices;
+    private List<Button> buttons;
+    private List<TextMeshProUGUI> texts;
+    public List<Image> images;
 
     private void Start()
     {
-        prices = new int[buttons.Length];
-
-        turretScript = turret.GetComponent<Turret>();
-        if (turretScript != null)
+        // Automatically find Turret gameobject in parent
+        if(turret == null)
         {
-            prices[0] = turretScript.sellCost;
-            prices[1] = turretScript.upgradeCost;
-            prices[2] = turretScript.repairCost;
+            turret = GetComponentInParent<Turret>();
+        }
 
-            for (int i = 0; i < texts.Length; i++)
+        Assert.IsNotNull(turret, "Turret UI could not find turret.");
+
+        buttons = new List<Button> { sellButton, upgradeButton, repairButton };
+        prices = new List<int> { turret.sellCost, turret.upgradeCost, turret.repairCost };
+        texts = buttons.Select(button => button.GetComponentInChildren<TextMeshProUGUI>(true)).ToList();
+        images = buttons.Select(button => button.GetComponent<Image>()).ToList();
+
+        SetButtonPriceText();
+        LinkButtonsToTurret();
+    }
+
+    private void LinkButtonsToTurret()
+    {
+        // Remove existing listeners
+        sellButton.onClick.RemoveAllListeners();
+        upgradeButton.onClick.RemoveAllListeners();
+        repairButton.onClick.RemoveAllListeners();
+        exitButton.onClick.RemoveAllListeners();
+
+        // Add new listeners
+        sellButton.onClick.AddListener(turret.SellTurret);
+        upgradeButton.onClick.AddListener(turret.UpgradeTurret);
+        repairButton.onClick.AddListener(turret.RepairTurret);
+        exitButton.onClick.AddListener(turret.DisableTurretUI);
+    }
+
+    private void SetButtonPriceText()
+    {
+        for (int i = 0; i < texts.Count; i++)
+        {
+            if (i == 0)
             {
-                if (i == 0)
-                {
-                    texts[i].SetText("-$" + prices[i]);
-                }
-                else
-                {
-                    texts[i].SetText("$" + prices[i]);
-                }
+                texts[i].SetText("-$" + prices[i]);
+            }
+            else
+            {
+                texts[i].SetText("$" + prices[i]);
             }
         }
     }
 
-    private void Update()
-    {
-        for (int i = 1; i < buttons.Length; i++)
-        {
-            buttons[i].GetComponent<Image>().sprite = Bank.instance.CanWithdrawMoney(prices[i]) ? canPurchaseSprite : cannotPurchaseSprite;
-        }
-    }
+	private void Update()
+	{
+		for (int i = 1; i < buttons.Count; i++)
+		{
+			images[i].sprite = Bank.instance.CanWithdrawMoney(prices[i]) ? canPurchaseSprite : cannotPurchaseSprite;
+		}
+	}
 }
