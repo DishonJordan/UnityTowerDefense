541455c66d95143b47dbc95ecf47ad622cb363ff
DishonJordan <dishonjordan@hotmail.com>
2020-04-30 23:07:43 -0700

    added upgrades and materials for Blaster

---
+++ Assets/Scripts/BallistaTurret.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class BallistaTurret : Turret
+{
+    private GameObject arrow;
+
+    private void Start()
+    {
+        arrow = this.gameObject.transform.GetChild(0).GetChild(0).gameObject;
+    }
+
+    /* Instantiates a projectile at the firepoint and sets the target of the projectile to the currentTarget */
+    protected override void FireProjectile()
+    {
+        if (currentTarget != null)
+        {
+            arrow.SetActive(true);
+            GameObject projectile = Instantiate(turretProjectile, firePoint.position, firePoint.rotation);
+            Projectile p = projectile.GetComponent<Projectile>();
+
+            p.SetTarget(currentTarget);
+            arrow.SetActive(false);
+        }
+    }
+}
