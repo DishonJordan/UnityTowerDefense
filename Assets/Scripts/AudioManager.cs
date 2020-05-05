1249f3895b28439bade28ca8fc6f7daa62c3590c
Kevin Huang <khuan028@ucr.edu>
2020-05-05 12:12:07 -0700

    Added Level Complete Music

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -23,9 +23,39 @@ public class AudioManager : MonoBehaviour
     }
 
     public AudioSource globalSFXAudioSource;
+    public AudioSource musicAudioSource;
+    public AudioClip levelMusic;
+    public AudioClip levelCompleteMusicHead;
+    public AudioClip levelCompleteMusicBody;
+    public float levelCompleteMusicTransitionOffset;
+
+    public void Start()
+    {
+        musicAudioSource.loop = true;
+        musicAudioSource.clip = levelMusic;
+        musicAudioSource.Play();
+    }
 
     public void PlayGlobalSFX(AudioClip clip)
     {
         globalSFXAudioSource.PlayOneShot(clip);
+    }
+
+    public void PlayLevelCompleteMusic()
+    {
+        StopAllCoroutines();
+        StartCoroutine(_PlayLevelCompleteMusic());
+    }
+
+    private IEnumerator _PlayLevelCompleteMusic()
+    {
+        musicAudioSource.loop = false;
+        musicAudioSource.clip = levelCompleteMusicHead;
+        musicAudioSource.Stop();
+        musicAudioSource.Play();
+        yield return new WaitForSeconds(levelCompleteMusicHead.length + levelCompleteMusicTransitionOffset);
+        musicAudioSource.clip = levelCompleteMusicBody;
+        musicAudioSource.loop = true;
+        musicAudioSource.Play();
     }
 }
