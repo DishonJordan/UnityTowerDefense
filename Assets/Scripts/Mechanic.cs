d89be64ba85d769567bff2ed1d9119bab4531c26
DishonJordan <dishonjordan@hotmail.com>
2020-05-06 18:44:16 -0700

    added basic animations to the mechanic

--- Assets/Scripts/Mechanic.cs
+++ Assets/Scripts/Mechanic.cs
@@ -22,13 +22,17 @@ public class Mechanic : MonoBehaviour
     private State state;
     private float timer;
     private MechanicManager mManager;
+    private float turnSpeed;
+    private Animator anim;
 
     private void Start()
     {
         state = State.Home;
         timer = 0.0f;
+        turnSpeed = 4f;
         homePosition = transform.position;
         mManager = MechanicManager.instance;
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -56,7 +60,6 @@ public class Mechanic : MonoBehaviour
                 }
                 break;
             case State.Finished:
-                //mManager.RemoveTask();
                 task = null;
                 RequestTask();
 
@@ -89,18 +92,21 @@ public class Mechanic : MonoBehaviour
         switch (state)
         {
             case State.Home:
+                anim.SetBool("Move", false);
                 RequestTask();
                 break;
             case State.MoveToTask:
-                MoveTowardsTarget(task.taskLocation);
+                TurnTowardsTarget(task.taskLocation);
                 break;
             case State.Working:
+                anim.SetBool("Move", false);
                 WorkOnTurret();
                 break;
             case State.Finished:
+                anim.SetBool("Move", false);
                 break;
             case State.MoveToHome:
-                MoveTowardsTarget(homePosition);
+                TurnTowardsTarget(homePosition);
                 break;
             default:
                 Debug.Log("Should not reach here");
@@ -111,7 +117,23 @@ public class Mechanic : MonoBehaviour
     /* Moves the mechanic towards the target */
     private void MoveTowardsTarget(Vector3 target)
     {
+        anim.SetBool("Move", true);
         transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.x, transform.position.y, target.z), movementSpeed * Time.deltaTime);
+    }
+
+    private void TurnTowardsTarget(Vector3 target)
+    {
+        Vector3 goalDirection = (target - transform.position).normalized;
+
+        if (Vector3.Dot(goalDirection, transform.forward) > 0.90f)
+        {
+            MoveTowardsTarget(target);
+        }
+        else
+        {
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, new Vector3(target.x, transform.position.y, target.z) - transform.position, turnSpeed * Time.deltaTime, 0.0f);
+            transform.rotation = Quaternion.LookRotation(newDirection);
+        }
     }
 
     /* Delay before task is performed */
