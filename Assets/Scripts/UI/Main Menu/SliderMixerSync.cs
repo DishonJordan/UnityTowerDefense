4019663d07ec8dfd036d50e7e114abffa2b29eec
Kevin Huang <khuan028@ucr.edu>
2020-05-12 13:47:16 -0700

    Added Options Submenu to Main Menu.

---
+++ Assets/Scripts/UI/Main Menu/SliderMixerSync.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class SliderMixerSync : MonoBehaviour
+{
+    private Slider slider;
+    private const float maxVol = 0;
+    private const float minVol = -80.0f;
+    public AudioMixer mixer;
+    public string mixerParam;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+        slider.onValueChanged.AddListener(OnSliderChanged);
+    }
+
+    private void OnEnable()
+    {
+        float mixerParamVal;
+        Assert.IsTrue(mixer.GetFloat(mixerParam, out mixerParamVal));
+        float t = Mathf.Clamp01(Mathf.InverseLerp(minVol, maxVol, mixerParamVal));
+        slider.value = t;
+    }
+
+
+    private void OnSliderChanged(float val)
+    {
+        float t = slider.value;
+        float mixerParamVal = Mathf.Lerp(minVol, maxVol, t);
+        Assert.IsTrue(mixer.SetFloat(mixerParam, mixerParamVal));
+    }
+}
