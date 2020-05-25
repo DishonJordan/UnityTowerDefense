698e62eb850860fea951f17b3b039b31bff3f92b
Kevin Huang <khuan028@ucr.edu>
2020-05-25 15:57:23 -0700

    Added level select menu

---
+++ Assets/Scripts/UI/AutoAspect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteAlways]
+public class AutoAspect : MonoBehaviour
+{
+    private Image image;
+    private AspectRatioFitter fitter;
+
+    void Update()
+    {
+        if(!image) image = GetComponent<Image>();
+        if(!fitter) fitter = GetComponent<AspectRatioFitter>();
+        if (!image || !fitter) return;
+        float estimatedAspect = image.sprite.rect.width / image.sprite.rect.height;
+        fitter.aspectRatio = estimatedAspect;
+    }
+}
